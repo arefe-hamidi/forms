cada0b92455689aca47fc3e698a6f3fa5b6734cd
arefe_hamidi <arefehamidi.guilan@gmail.com>
2023-08-25 14:52:22 +0330

    add use input hook

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,12 +1,45 @@
+import useInput from "../hooks/use-input";
 const SimpleInput = (props) => {
+  const {
+    value: enteredName,
+    isValid: enteredNameIsValid,
+    hasError: nameInputHasError,
+    valueChangeHandler: nameInputChangeHandler,
+    valueBlurHandler: nameInputBlurHandler,
+    reset: resetNameInput,
+  } = useInput((value) => value.trim() !== "");
+  let formIsValid = false;
+  if (enteredNameIsValid) {
+    formIsValid = true;
+  }
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    if (!enteredNameIsValid) {
+      return;
+    }
+    console.log(enteredName);
+    resetNameInput();
+  };
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div
+        className={!nameInputHasError ? "form-control" : "form-control invalid"}
+      >
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onBlur={nameInputBlurHandler}
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
+        {nameInputHasError && (
+          <p className="error-text">Name must not be empty.</p>
+        )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
